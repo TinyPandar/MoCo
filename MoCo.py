80cdd91396af5fa0a0ada46a39f6b8b1de564d69
lilp <1176680857@qq.com>
2024-09-15 17:52:33 +0800

    feat: 修复bug并且添加了多卡训练和shuffleBN

--- MoCo.py
+++ MoCo.py
@@ -13,14 +13,23 @@ class MoCo(nn.Module):
         self.encoder_q = torchvision.models.resnet50(num_classes=dim)
         self.encoder_k = torchvision.models.resnet50(num_classes=dim)
         # queue is trans F*Q
-        self.register_buffer("queue", torch.randn(dim, Q))
+        self.queue = torch.randn(dim, Q).cuda()
+        # 非常影响初始化
+        self.queue = nn.functional.normalize(self.queue, dim=0)
         self.start = 0
         self.Q = Q
         self.momentum = momentum
         self.T = T
+        for para in zip(self.encoder_q.parameters(), self.encoder_k.parameters()):
+            para[1].data = para[0].data
+            para[1].requires_grad = False
 
+    @torch.no_grad()
     def update_queue(self, k):
+        k = concat_all_gather(k)
+        print(self.start)
         batch_size = k.shape[0]
+        assert self.start % batch_size == 0  # for simplicity
         self.queue[:, self.start:self.start + batch_size] = k.T
         self.start = (self.start + batch_size) % self.Q
 
@@ -28,14 +37,53 @@ class MoCo(nn.Module):
         for para in zip(self.encoder_q.parameters(), self.encoder_k.parameters()):
             para[1].data = self.momentum * para[1].data + (1 - self.momentum) * para[0].data
 
+    @torch.no_grad()
+    def batch_shuffle_ddp(self, x):
+        batch_size_this = x.shape[0]
+        x_gather = concat_all_gather(x)
+        batch_size_all = x_gather.shape[0]
+
+        num_gpus = batch_size_all // batch_size_this
+
+        # 生成随机排布作为当前位置经过shuffule后的数据
+        idx_shuffle = torch.randperm(batch_size_all).cuda()
+
+        # broadcast to all gpus
+        torch.distributed.broadcast(idx_shuffle, src=0)
+
+        # 根据新索引排序index，就能找到原来的值去了哪
+        idx_unshuffle = torch.argsort(idx_shuffle)
+
+        # 根据排布重新分配值
+        gpu_idx = torch.distributed.get_rank()
+        idx_this = idx_shuffle.view(num_gpus, -1)[gpu_idx]
+
+        return x_gather[idx_this], idx_unshuffle
+
+    @torch.no_grad()
+    def batch_unshuffle_ddp(self, x, idx_unshuffle):
+        batch_size_this = x.shape[0]
+        x_gather = concat_all_gather(x)
+        batch_size_all = x_gather.shape[0]
+
+        # 从所有被shuffule的数据上恢复
+        num_gpus = batch_size_all // batch_size_this
+        gpu_idx = torch.distributed.get_rank()
+        # 获取当前gpu原有的数据
+        idx_this = idx_unshuffle.view(num_gpus, -1)[gpu_idx]
+
+        return x_gather[idx_this]
+
     def forward(self, im_q, im_k):
         q = self.encoder_q(im_q)
         q = nn.functional.normalize(q, dim=-1)
 
         with torch.no_grad():
             self.momentum_update()
+            im_k, shuffle_idx = self.batch_shuffle_ddp(im_k)
             k = self.encoder_k(im_k)
             k = nn.functional.normalize(k, dim=-1)
+            k = self.batch_unshuffle_ddp(k, shuffle_idx)
 
         # 一开始直接用的torch.mm和torch.bmm
         # N here means batch size
@@ -51,8 +99,21 @@ class MoCo(nn.Module):
         logits /= self.T
         # pos 0 is k and the pos of similarity that should close to 1, others kept to zero
         # 第一次用的默认，报错了。改成long
-        label = torch.zeros(logits.shape[0], dtype=torch.long)
+        label = torch.zeros(logits.shape[0], dtype=torch.long).cuda()
 
         self.update_queue(k)
 
         return logits, label
+
+
+@torch.no_grad()
+def concat_all_gather(tensor):
+    # 创建gpu_num个batch的数组
+    tensors_gather = [
+        torch.ones_like(tensor) for _ in range(torch.distributed.get_world_size())
+    ]
+    # 获取数据
+    torch.distributed.all_gather(tensors_gather, tensor, async_op=False)
+
+    output = torch.cat(tensors_gather, dim=0)
+    return output
--- train.py
+++ train.py
@@ -5,14 +5,52 @@ from torch import optim
 from torchvision import transforms
 from tqdm import tqdm
 from MoCo import MoCo
-from torchvision.datasets import CIFAR10
-from torch.utils.data import DataLoader
+import torchvision.datasets as datasets
+from torch.utils.data import DataLoader, DistributedSampler
+import torch.distributed as dist
+from torch.nn.parallel import DistributedDataParallel as DDP
+import os
 
 
-def train():
-    train_dataset = CIFAR10('datasets', train=True, download=True, transform=transforms.ToTensor())
-    train_loader = DataLoader(train_dataset, batch_size=32, shuffle=True)
-    model = MoCo(momentum=0.01, dim=128, T=0.07, Q=64)
+class TwoCropsTransform:
+    """Take two random crops of one image as the query and key."""
+
+    def __init__(self, base_transform):
+        self.base_transform = base_transform
+
+    def __call__(self, x):
+        q = self.base_transform(x)
+        k = self.base_transform(x)
+        return [q, k]
+
+
+def accuracy(output, target, topk=(1,)):
+    """Computes the accuracy over the k top predictions for the specified values of k"""
+    with torch.no_grad():
+        maxk = max(topk)
+        batch_size = target.size(0)
+
+        _, pred = output.topk(maxk, 1, True, True)
+        pred = pred.t()
+        correct = pred.eq(target.view(1, -1).expand_as(pred))
+
+        res = []
+        for k in topk:
+            correct_k = correct[:k].reshape(-1).float().sum(0, keepdim=True)
+            res.append(correct_k.mul_(100.0 / batch_size))
+        return res
+
+
+def setup_distributed(rank, world_size):
+    os.environ['MASTER_ADDR'] = 'localhost'
+    os.environ['MASTER_PORT'] = '12355'
+    dist.init_process_group("nccl", rank=rank, world_size=world_size)
+
+
+def train(rank, world_size):
+    setup_distributed(rank, world_size)
+    torch.cuda.set_device(rank)
+
     normalize = transforms.Normalize(
         mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]
     )
@@ -21,31 +59,45 @@ def train():
         transforms.RandomGrayscale(p=0.2),
         transforms.ColorJitter(0.4, 0.4, 0.4, 0.4),
         transforms.RandomHorizontalFlip(),
+        transforms.ToTensor(),
         normalize,
     ]
-    random_transform = transforms.Compose(augmentation)
 
-    optimizer = optim.SGD(model.parameters(), lr=0.1, momentum=0.9, weight_decay=5e-4)
-    criterion = torch.nn.CrossEntropyLoss()
+    train_dataset = datasets.ImageFolder(
+        'imagenet/train', TwoCropsTransform(transforms.Compose(augmentation))
+    )
 
-    # training
+    train_sampler = DistributedSampler(train_dataset, num_replicas=world_size, rank=rank, shuffle=True)
+    train_loader = DataLoader(train_dataset, batch_size=64, sampler=train_sampler, drop_last=True)
+
+    model = MoCo(momentum=0.999, dim=128, T=0.07, Q=65536).cuda()
+    model = DDP(model, device_ids=[rank])
+
+    optimizer = optim.SGD(model.parameters(), lr=0.0075, momentum=0.9, weight_decay=1e-4)
+    criterion = torch.nn.CrossEntropyLoss().cuda()
+
     model.train()
-    for epoch in tqdm(range(100)):
-        # dataloader只能处理tensor。dataset加了transform
-        for img, _ in tqdm(train_loader):
-            # 一开始在想怎么用一个dataloader读两个数据，后来仔细看了论文发现是两次变换
-            img_q = random_transform(img)
-            img_k = random_transform(img)
+    for epoch in range(100):
+        train_sampler.set_epoch(epoch)
+        for batch, (img, _) in enumerate(train_loader):
+            img_q = img[0].cuda(rank, non_blocking=True)
+            img_k = img[1].cuda(rank, non_blocking=True)
 
             out, label = model(img_q, img_k)
-
             loss = criterion(out, label)
-            acc = accuracy_score(out.argmax(), label)
-            print(f"loss:{loss.item()} acc:{acc}")
+
+            top1, top5 = accuracy(out, label, topk=(1, 5))
+            if rank == 0:
+                print(f"epoch:{epoch} batch:{batch} loss:{loss.item()} top1:{top1.item()}, top5:{top5.item()}")
+
             optimizer.zero_grad()
             loss.backward()
             optimizer.step()
 
+        if rank == 0:
+            torch.save(model.module, f'MoCo-DDP-{epoch}.pth')
+
 
 if __name__ == '__main__':
-    train()
+    world_size = torch.cuda.device_count()
+    torch.multiprocessing.spawn(train, args=(world_size,), nprocs=world_size, join=True)
